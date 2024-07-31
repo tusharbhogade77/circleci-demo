dbebdcf535f1ddba7a677b68c8524eb47ebd77d3
Tushar Bhogade <tusharbhogade7777@gmail.com>
2024-07-31 22:30:12 +0530

    testing library added

--- App.tsx
+++ App.tsx
@@ -1,63 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React from "react";
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
+  Button,
+  TextInput,
   View,
-} from 'react-native';
+} from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const App = () => {
+  const [message, setMessage] = React.useState();
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
+  const isDarkMode = useColorScheme() === "dark";
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -65,53 +23,33 @@ function App(): JSX.Element {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+
+      <Button
+        title="Say Hello"
+        onPress={() => {
+          setTimeout(() => {
+            setMessage("Hello Tester");
+          }, Math.floor(Math.random() * 200));
+        }}
+      />
+      {message && (
+        <Text style={styles.messageText} testID="printed-message">
+          {message}
+        </Text>
+      )}
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  messageText: {
+    fontFamily: "Arial",
+    fontSize: 38,
+    textAlign: "center",
+    marginTop: 10,
   },
 });
 
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,17 @@
-/**
- * @format
- */
+import "react-native";
+import React from "react";
+import App from "../App";
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import { fireEvent, render, waitFor } from "@testing-library/react-native";
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+it("Renders Message", async () => {
+  const { getByTestId, getByText, queryByTestId, toJSON } = render(<App />);
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  const button = getByText("Say Hello");
+  fireEvent.press(button);
+
+  await waitFor(() => expect(queryByTestId("printed-message")).toBeTruthy());
+
+  expect(getByTestId("printed-message").props.children).toBe("Hello Tester");
+  expect(toJSON()).toMatchSnapshot();
 });
